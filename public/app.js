c7f37db3ff2add60d83e4db897732852f7207c81
kiraci <>
2021-06-07 17:30:29 +0300

    Class

--- public/app.js
+++ public/app.js
@@ -1,5 +1,23 @@
 "use strict";
-//const form = document.querySelector('form')!;
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// invoices.push({ name: 'shaun' });
+console.log(invoices);
 var form = document.querySelector('.new-item-form');
 console.log(form.children);
 // inputs
