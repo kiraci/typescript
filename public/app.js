b9fd7904b40c9d952eff0ad7c1a9f8d8bac3bf27
kiraci <>
2021-06-07 23:53:54 +0300

    Rendering and generics

--- public/app.js
+++ public/app.js
@@ -1,30 +1,6 @@
 import { Invoice } from './classes/Invoice.js';
-const me = {
-    name: 'shaun',
-    //age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent ', amount);
-        return amount;
-    },
-};
-console.log(me);
-me.speak('hello, world');
-const greetPerson = (person) => {
-    console.log('hello ', person.name);
-};
-greetPerson(me);
-//greetPerson({name: 'shaun'});
-const invOne = new Invoice('maria', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
 // inputs
@@ -32,7 +8,44 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
+// GENERICS
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+//let docTwo = addUID('shaun');
+console.log(docOne.name);
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { name: 'shaun' }
+};
+const docFour = {
+    uid: 1,
+    resourceName: 'shoppingList',
+    data: ['bread', 'milk']
+};
+console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,73 @@
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-// interfaces
-interface IsPerson {
-    name: string;
-    age?: number;
-    speak(a: string): void;
-    spend(a: number): number;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
   
-  const me: IsPerson = {
-    name: 'shaun',
-    //age: 30,
-    speak(text: string): void {
-      console.log(text);
-    },
-    spend(amount: number): number {
-      console.log('I spent ', amount);
-      return amount;
-    },
+  list.render(doc, type.value, 'end');
+});
+
+// GENERICS
+
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+const addUID = <T extends {name: string}>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+  }
+  
+  let docOne = addUID({name: 'yoshi', age: 40});
+  //let docTwo = addUID('shaun');
+  
+  console.log(docOne.name);
+  
+  // with interfaces
+  interface Resource<T> {
+    uid: number;
+    resourceName: string;
+    data: T;
+  }
+  
+  const docThree: Resource<object> = {
+    uid: 1, 
+    resourceName: 'person', 
+    data: { name: 'shaun' }
   };
   
-  console.log(me);
-  me.speak('hello, world');
+  const docFour: Resource<string[]> = {
+    uid: 1, 
+    resourceName: 'shoppingList', 
+    data: ['bread', 'milk']
+  };
   
-  const greetPerson = (person: IsPerson): void => {
-    console.log('hello ', person.name);
-  }
-  
-  greetPerson(me);
-  //greetPerson({name: 'shaun'});
-
-const invOne = new Invoice('maria', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-  
-  let invoices: Invoice[] = [];
-  invoices.push(invOne)
-  invoices.push(invTwo);
-  
-  invoices.forEach(inv => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-  })
-  
-  
-  
-  const form = document.querySelector('.new-item-form') as HTMLFormElement;
-  console.log(form.children);
-  
-  // inputs
-  const type = document.querySelector('#type') as HTMLInputElement;
-  const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
-  const details = document.querySelector('#details') as HTMLInputElement;
-  const amount = document.querySelector('#amount') as HTMLInputElement;
-  
-  form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
-  
-    console.log(
-      type.value, 
-      tofrom.value, 
-      details.value, 
-      amount.valueAsNumber
-    );
-  });
+  console.log(docThree, docFour);
