439fedc17da9ca521cd53287aab213ffbba3dd10
kiraci <>
2021-06-07 15:53:11 +0300

    all types

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,26 @@
-var func = function (diameter) {
-    return diameter * Math.PI;
+//types
+var info = "hello";
+var age = 23;
+var correct = true;
+var stringArray = [];
+stringArray.push("hi");
+var mixed = [];
+mixed.push("I am ");
+mixed.push(20);
+mixed.push(true);
+console.log(mixed);
+var uid;
+uid = "typescript";
+uid = 30;
+var team;
+team = {
+    name: "galatasaray",
+    date: 1905
 };
-console.log(func(53));
+team = [];
+var team2;
+team2 = {
+    name: "asd",
+    age: 12,
+    isChampion: false
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,36 @@
-const func = (diameter : number) => {
-    return diameter * Math.PI;
-};
+//types
+let info : string = "hello";
+let age : number = 23;
+let correct : boolean = true;
 
-console.log( func(53) );
+let stringArray : string[] = [];
+stringArray.push("hi");
+
+let mixed : (string | number | boolean)[] = [];
+mixed.push("I am ");
+mixed.push(20);
+mixed.push(true);
+console.log(mixed);
+
+let uid : string | number;
+uid = "typescript";
+uid = 30;
+
+let team : object;
+team = {
+    name : "galatasaray",
+    date : 1905
+}
+team = [];
+
+let team2 : {
+    name : string,
+    age : number,
+    isChampion : boolean
+}
+
+team2 = {
+    name : "asd",
+    age : 12,
+    isChampion : false
+}
