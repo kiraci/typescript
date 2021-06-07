5a4d7497b6e27a19d52aa73f3f79cf9042cf89f8
kiraci <>
2021-06-07 17:20:15 +0300

    DOM

--- public/sandbox.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var logDetails;
-logDetails = function (user, item) {
-    console.log("Item is " + item + ". UID is " + user.uid);
-};
-logDetails({ name: "ahmet", uid: 5 }, "sabun");
