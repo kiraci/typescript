2d856aefc69389a13deb8c4e9c0a4de235125fa3
kiraci <>
2021-06-07 16:49:07 +0300

    functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,16 @@
 "use strict";
-var dynamicvar;
-dynamicvar = "asd";
-dynamicvar = 14;
+var greet;
+greet = function () {
+    console.log("Merhaba");
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 'ninja');
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,19 @@
-let dynamicvar : any;
+let greet : Function;
 
-dynamicvar = "asd";
+greet = () => {
+    console.log("Merhaba");
+}
 
-dynamicvar = 14;
+const add = (a: number, b: number, c: number | string = 10): void => {
+    console.log(a + b);
+    console.log(c);
+  }
+  
+  add(5, 10, 'ninja');
+  
+  const minus = (a: number, b: number): number => {
+    return a + b;
+  }
+  
+  let result = minus(10,7);
+  console.log(result);
